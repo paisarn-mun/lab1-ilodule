c9a27c29878b630a5a46927deb504ca3ea28f533
ilodule <129973134+ilodule@users.noreply.github.com>
2024-09-19 14:25:52 +0700

    Update exercise 3.c

--- exercise 3.c
+++ exercise 3.c
@@ -1,5 +1,7 @@
-// The modulus operator #include <stdio.h>
-int main (void) {
-// Please input your code here
-return 0;
-}
+1. HTTP protocol
+2. The time is : 4.570622 
+3. First question : 128.119.245.12 
+   Second question : 10.62.4.75
+4. Browser : Edge
+5. Destination Number Port : 88
+6. The pictures are in the folder
